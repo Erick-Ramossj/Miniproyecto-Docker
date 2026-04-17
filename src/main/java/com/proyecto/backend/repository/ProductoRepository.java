74557333aa9f09ad7863e46d9ccf763c43248d91
Erick-Ramossj <erick.ramos@vallegrande.edu.pe>
2026-04-16 20:41:48 -0500

    estructura corregida

--- src/main/java/com/proyecto/backend/repository/ProductoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.proyecto.backend.repository;
-
-import com.proyecto.backend.model.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductoRepository extends JpaRepository<Producto, Integer> {
-}
